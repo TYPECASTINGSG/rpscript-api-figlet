bcefbd13cbb9b222ae5e32472806d3f1f31b6f21
James Chong <wei3hua2@gmail.com>
2018-07-16 19:01:54 +0800

    include documentation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-/**
- * @module Figlet
- */
-
 var figlet = require('figlet');
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
+/** Welcome to Basic Module
+ * @namespace Figlet
+*/
 @RpsModule("figlet")
 export default class RPSFiglet {
 
+  /**
+ * @function figlet
+ * @memberof Figlet
+ * @example
+ *
+ * figlet 'Boo!' --font="Ghost"
+ * 
+ * @param {string} text data to be printed out.
+ * @returns {string} the formatted display.
+ * @summary Display Ascii Art
+ * @description Figlet. Ascii Art
+ * 
+ * @see {@link https://www.npmjs.com/package/figlet}
+ * 
+*/
   @rpsAction({verbName:'figlet'})
   async figlet (ctx:RpsContext,opts:Object, text:string) : Promise<string>{
     return figlet.textSync(text,opts);
